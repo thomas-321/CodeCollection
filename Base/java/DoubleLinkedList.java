b936dcbecf2b7bdff440312f13a036f716af321b
Thomas van Vliet <113383928+thomas-321@users.noreply.github.com>
2023-06-05 16:40:51 +0200

    Added functions which return the item in the requested index

--- Base/java/DoubleLinkedList.java
+++ Base/java/DoubleLinkedList.java
@@ -14,6 +14,46 @@ public class DoubleLinkedList<T> {
     DoubleLinkedList() {}
 
     /**
+     * Function splits the list in 2 and depending on the requested index and list size 
+     * decides to loop from the front or from the back
+     * @throws IndexOutOfBoundsException when the list is empty
+    */
+    public T get(int index){
+        if (first == null || last == null || !(index < size)){
+            throw new IndexOutOfBoundsException();
+        }
+        if (index < size / 2){
+            return getFromFront(index);
+        } else {
+            return getFromBack(index);
+        }
+    }
+
+    /**
+     * Function starts looping at the first node in the list until the requested index is reached
+     * @return object located in the requested index
+    */
+    private T getFromFront(int index){
+        Node<T> node = first;
+        for (int counter = 0; counter < index; counter++ ){
+            node = node.next;
+        }
+        return node.object;
+    }
+
+    /**
+     * Function starts looping at the last node in the list until the requested index is reached
+     * @return object located in the requested index
+    */
+    private T getFromBack(int index){
+        Node<T> node = last;
+        for (int counter = size-1; counter > index; counter-- ){
+            node = node.back;
+        }
+        return node.object;
+    }
+
+    /**
      * @return first element in the list
      * @throws IndexOutOfBoundsException when the item is null
     */
